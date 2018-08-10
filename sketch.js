0ca524342d88c60fd78e6fd32deb367134caee70
HatchFriday <dilshan.gunasekera@hatchcanada.com>
2018-08-10 13:02:17 -0400

    fixed canvas offsets

--- sketch.js
+++ sketch.js
@@ -10,16 +10,46 @@
 // Global variables
 var cx;
 
+function getOffset( el ) {
+    var _x = 0;
+    var _y = 0;
+    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+        _x += el.offsetLeft - el.scrollLeft;
+        _y += el.offsetTop - el.scrollTop;
+        el = el.offsetParent;
+    }
+    return { top: _y, left: _x };
+}
+
+
 function setup() {
     // make the canvas 400 x 400 large
     canvas_element = createCanvas(400, 400); /* ONLY CHANGE THIS LINE TO CHANGE CANVAS DIMENSIONS */
 
     // HTML Styling canvas
-    canvas_element.style('display', 'flex');
-    canvas_element.style('border','1px solid #000000');
     canvas_element.parent('canvas-place-holder');
-    console.log(canvas_element.parent.size());
+    canvas_element.style('display', 'block');
+    canvas_element.style('border','1px solid #000000');
 
+    let x = getOffset( document.getElementById('canvas-place-holder') ).left;
+    let y = getOffset( document.getElementById('canvas-place-holder') ).top;
+    let element = document.getElementById('canvas-place-holder');
+    let positionInfo = element.getBoundingClientRect();
+    let w = positionInfo.left;
+    cx = w - width / 2;
+    canvas_element.position(cx + x, y);
+
+}
+
+function windowResized() {
+    let x = getOffset( document.getElementById('canvas-place-holder') ).left;
+    let y = getOffset( document.getElementById('canvas-place-holder') ).top;
+    var element = document.getElementById('canvas-place-holder');
+    var positionInfo = element.getBoundingClientRect();
+    var w = positionInfo.left;
+    console.log(w);
+    cx = x + (w - width / 2);
+    canvas_element.position(cx, y);
 }
 
 function draw() {
